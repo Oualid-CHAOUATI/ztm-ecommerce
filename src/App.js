73c015e634b48f12d0fe6f3e9e5ab65a6a1a05df
waleed <oualidchaouati3@gmail.com>
2023-05-05 17:19:08 +0200

    init and add sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imgSrc:
+        "https://images.unsplash.com/photo-1572307480813-ceb0e59d8325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imgSrc:
+        "https://images.unsplash.com/photo-1605908502724-9093a79a1b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="categories-wrapper">
+        {categories.map((category) => (
+          <div
+            className="category"
+            style={{ backgroundImage: `url(${category.imgSrc})` }}
+          >
+            <h2 className="category__name">{category.title}</h2>
+            <button>Shop now!</button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
