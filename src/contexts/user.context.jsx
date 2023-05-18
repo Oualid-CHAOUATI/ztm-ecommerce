e7da5ca9c300a41db83802c1f04411a79e198838
waleed <oualidchaouati3@gmail.com>
2023-05-18 02:27:40 +0200

    implement reducer in UserContext

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 
 import { onAuthStateChangedListener,createUserDocFromAuth } from "../utils/firebase/firebase.utils";
 
@@ -8,14 +8,28 @@ export const UserContext= createContext({
     setCurrentUser:()=>null
 }) ;
 
+const USER_REDUCER_ACTION_TYPES= {SET_CURRENT_USER:"SET_CURRENT_USER"}
 
+const userReducer = (state,action)=>{
+    console.log("dispatched")
+    console.log(action)
+;
+const {type,payload}=action;
 
+switch(type){
+     case USER_REDUCER_ACTION_TYPES.SET_CURRENT_USER:return {currentUser:payload}
+     default: throw new Error ('Invalid action type in user Reducer');
+}
+}
 
+const INITIAL_STATE={currentUser:null};
 export const UserProvider=({children})=>{
 
-const [currentUser,setCurrentUser] = useState(null);
-const value={currentUser,setCurrentUser};
+const [{currentUser},dispatch]=useReducer(userReducer,INITIAL_STATE);
 
+console.log(currentUser);
+const setCurrentUser=(user)=>dispatch({type:USER_REDUCER_ACTION_TYPES.SET_CURRENT_USER,payload:user});
+const value={currentUser,setCurrentUser};
 
 useEffect(()=>{
 
